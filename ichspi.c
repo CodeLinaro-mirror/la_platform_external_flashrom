a06119969d0c816fb8f8566dc27a2b4e3dbf2863
Alexander Goncharov <chat@joursoir.net>
2022-07-29 09:05:14 +0300

    UPSTREAM: tree: provide flashrom context into programmer_delay()
    Modify the `programmer_delay` function signature to allow passing
    the flashrom context. Programmers that depend on internal delay
    should provide NULL as a context. The use of this function parameter
    will be introduced in CB:67393.
    
    TOPIC=programmer_handle_global
    TEST=builds
    
    (cherry picked from commit 5c69cde56114ec96310a27dca7e72617d15ba2bc)
    
    Original-Change-Id: Ibb0bce26ce2052853ee52158d7ba742967a9e229
    Original-Signed-off-by: Alexander Goncharov <chat@joursoir.net>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/66373
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    Original-Reviewed-by: Felix Singer <felixsinger@posteo.net>
    GitOrigin-RevId: 5c69cde56114ec96310a27dca7e72617d15ba2bc
    Change-Id: Iaaac7dba36c131945e1940edcdc55cea6e86fb73
    Signed-off-by: CopyBot <copybot.service@gmail.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/3959955
    Reviewed-by: Evan Benn <evanbenn@chromium.org>
    Tested-by: Evan Benn <evanbenn@chromium.org>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

--- ichspi.c
+++ ichspi.c
@@ -878,7 +878,7 @@ static int ich7_run_opcode(OPCODE op, uint32_t offset,
 
 	timeout = 100 * 60;	/* 60 ms are 9.6 million cycles at 16 MHz. */
 	while ((REGREAD16(ICH7_REG_SPIS) & SPIS_SCIP) && --timeout) {
-		programmer_delay(10);
+		programmer_delay(NULL, 10);
 	}
 	if (!timeout) {
 		msg_perr("Error: SCIP never cleared!\n");
@@ -954,7 +954,7 @@ static int ich7_run_opcode(OPCODE op, uint32_t offset,
 	/* Wait for Cycle Done Status or Flash Cycle Error. */
 	while (((REGREAD16(ICH7_REG_SPIS) & (SPIS_CDS | SPIS_FCERR)) == 0) &&
 	       --timeout) {
-		programmer_delay(10);
+		programmer_delay(NULL, 10);
 	}
 	if (!timeout) {
 		msg_perr("timeout, ICH7_REG_SPIS=0x%04x\n", REGREAD16(ICH7_REG_SPIS));
@@ -994,7 +994,7 @@ static int ich9_run_opcode(OPCODE op, uint32_t offset,
 
 	timeout = 100 * 60;	/* 60 ms are 9.6 million cycles at 16 MHz. */
 	while ((REGREAD8(swseq_data.reg_ssfsc) & SSFS_SCIP) && --timeout) {
-		programmer_delay(10);
+		programmer_delay(NULL, 10);
 	}
 	if (!timeout) {
 		msg_perr("Error: SCIP never cleared!\n");
@@ -1074,7 +1074,7 @@ static int ich9_run_opcode(OPCODE op, uint32_t offset,
 	/* Wait for Cycle Done Status or Flash Cycle Error. */
 	while (((REGREAD32(swseq_data.reg_ssfsc) & (SSFS_FDONE | SSFS_FCERR)) == 0) &&
 	       --timeout) {
-		programmer_delay(10);
+		programmer_delay(NULL, 10);
 	}
 	if (!timeout) {
 		msg_perr("timeout, REG_SSFS=0x%08x\n", REGREAD32(swseq_data.reg_ssfsc));
@@ -1424,7 +1424,7 @@ static int ich_hwseq_wait_for_cycle_complete(unsigned int len, enum ich_chipset 
 	while ((((hsfs = REGREAD16(ICH9_REG_HSFS)) &
 		 (HSFS_FDONE | HSFS_FCERR)) == 0) &&
 	       --timeout_us) {
-		programmer_delay(8);
+		programmer_delay(NULL, 8);
 	}
 	REGWRITE16(ICH9_REG_HSFS, REGREAD16(ICH9_REG_HSFS));
 	if (!timeout_us) {
