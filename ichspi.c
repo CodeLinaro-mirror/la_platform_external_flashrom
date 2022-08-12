3076c1422705a46d06ef6a06efa003c842a65269
Edward O'Callaghan <quasisec@google.com>
2022-08-12 14:16:14 +1000

    UPSTREAM: ichspi.c: Allow passing programmer_cfg directly
    Modify the type signature of the programmer entry-point
    xxx_init() functions to allow for the consumption of the
    programmer parameterisation string data.
    Also plumb programmer_cfg though get_params.
    
    (cherry picked from commit a20ceffa352f790f14361211a1c9931475ec7ac7)
    
    Original-Change-Id: I480589bb50b47fdf5af259d068f49fedfce88ea5
    Original-Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/66661
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Original-Reviewed-by: Felix Singer <felixsinger@posteo.net>
    GitOrigin-RevId: a20ceffa352f790f14361211a1c9931475ec7ac7
    Change-Id: I9e874d55f0dab9efda1356d58ee7a002777c60e6
    Signed-off-by: CopyBot <copybot.service@gmail.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/3879125
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Tested-by: Peter Marheine <pmarheine@chromium.org>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    Commit-Queue: Peter Marheine <pmarheine@chromium.org>

--- ichspi.c
+++ ichspi.c
@@ -2110,9 +2110,9 @@ enum ich_spi_mode {
 	ich_swseq
 };
 
-static int get_ich_spi_mode_param(enum ich_spi_mode *ich_spi_mode)
+static int get_ich_spi_mode_param(const struct programmer_cfg *cfg, enum ich_spi_mode *ich_spi_mode)
 {
-	char *const arg = extract_programmer_param_str(NULL, "ich_spi_mode");
+	char *const arg = extract_programmer_param_str(cfg, "ich_spi_mode");
 	if (!arg) {
 		return 0;
 	} else if (!strcmp(arg, "hwseq")) {
@@ -2202,7 +2202,7 @@ static void init_chipset_properties(struct swseq_data *swseq, struct hwseq_data 
 	}
 }
 
-static int init_ich_default(void *spibar, enum ich_chipset ich_gen)
+static int init_ich_default(const struct programmer_cfg *cfg, void *spibar, enum ich_chipset ich_gen)
 {
 	unsigned int i;
 	uint16_t tmp2;
@@ -2215,7 +2215,7 @@ static int init_ich_default(void *spibar, enum ich_chipset ich_gen)
 
 	init_chipset_properties(&swseq_data, &hwseq_data, &num_freg, &num_pr, &reg_pr0, ich_gen);
 
-	int ret = get_ich_spi_mode_param(&ich_spi_mode);
+	int ret = get_ich_spi_mode_param(cfg, &ich_spi_mode);
 	if (ret)
 		return ret;
 
@@ -2450,7 +2450,7 @@ static int init_ich_default(void *spibar, enum ich_chipset ich_gen)
 	return 0;
 }
 
-int ich_init_spi(void *spibar, enum ich_chipset ich_gen)
+int ich_init_spi(const struct programmer_cfg *cfg, void *spibar, enum ich_chipset ich_gen)
 {
 	ich_generation = ich_gen;
 	ich_spibar = spibar;
@@ -2462,7 +2462,7 @@ int ich_init_spi(void *spibar, enum ich_chipset ich_gen)
 		return init_ich7_spi(spibar, ich_gen);
 	case CHIPSET_ICH8:
 	default:	/* Future version might behave the same */
-		return init_ich_default(spibar, ich_gen);
+		return init_ich_default(cfg, spibar, ich_gen);
 	}
 }
 
