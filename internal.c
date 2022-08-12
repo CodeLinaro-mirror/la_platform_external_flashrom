fbacb441ececfc581cbf2b7186284a90309a3fa8
Edward O'Callaghan <quasisec@google.com>
2022-08-12 15:42:29 +1000

    UPSTREAM: tree: plumb programmer_cfg into chipset_flash_enable()
    (cherry picked from commit 162b997f2fb36d44640c6ca97baee328550c0869)
    
    Original-Change-Id: I963c674d212ce791ee155020fa97bcf26cefca0c
    Original-Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/66673
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Original-Reviewed-by: Felix Singer <felixsinger@posteo.net>
    GitOrigin-RevId: 162b997f2fb36d44640c6ca97baee328550c0869
    Change-Id: I2602444bc10f32a9509637dbfe2f02f59af7674e
    Signed-off-by: CopyBot <copybot.service@gmail.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/3879126
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    Tested-by: Peter Marheine <pmarheine@chromium.org>
    Commit-Queue: Peter Marheine <pmarheine@chromium.org>

--- internal.c
+++ internal.c
@@ -297,7 +297,7 @@ int internal_init(const struct programmer_cfg *cfg)
 	/* try to enable it. Failure IS an option, since not all motherboards
 	 * really need this to be done, etc., etc.
 	 */
-	ret = chipset_flash_enable();
+	ret = chipset_flash_enable(cfg);
 	if (ret == -2) {
 		msg_perr("WARNING: No chipset found. Flash detection "
 			 "will most likely fail.\n");
