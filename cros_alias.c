0980027451bf022096cd39b69834c6069515675a
Edward O'Callaghan <quasisec@google.com>
2022-09-07 22:21:39 +1000

    BACKPORT: drivers/: Make 'fallback_{un}map' the default unless defined
    Drop the explicit need to specify the default 'fallback_{un}map'
    callback function pointer from the 'programmer_entry' struct.
    This is a reasonable default for every other driver in the tree
    with only a select few exceptions [atavia, serprog, dummyflasher
    and internal].
    
    Thus this simplifies driver development and paves way
    to remove the 'programmer' global handle.
    
    BUG=b:175472467,b:171093672
    
    BACKPORT required for cros_ec_dev.c and cros_alias.c
    
    Original-Change-Id: I5ea7bd68f7ae2cd4af9902ef07255ab6ce0bfdb3
    Original-Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/67404
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Felix Singer <felixsinger@posteo.net>
    Original-Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Original-Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    (cherry picked from commit 67d50156170b17e5bca460ab6e5648e2b11f061c)
    
    Change-Id: I708346d3433021c7bb21c41602b4666dbab5dc58
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/3944253
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Tested-by: Peter Marheine <pmarheine@chromium.org>
    Commit-Queue: Peter Marheine <pmarheine@chromium.org>

--- cros_alias.c
+++ cros_alias.c
@@ -28,8 +28,6 @@ const struct programmer_entry programmer_google_host_alias = {
 	.type			= OTHER,
 	.devs.note		= "Google host alias mechanism.\n",
 	.init			= cros_host_alias_init,
-	.map_flash_region	= physmap,
-	.unmap_flash_region	= physunmap,
 
 	/*
 	 * "Internal" implies in-system programming on a live system, so
--- cros_ec_dev.c
+++ cros_ec_dev.c
@@ -601,8 +601,6 @@ const struct programmer_entry programmer_cros_ec = {
 	.type			= OTHER,
 	.devs.note		= "Google EC.\n",
 	.init			= cros_ec_probe_dev,
-	.map_flash_region	= fallback_map,
-	.unmap_flash_region	= fallback_unmap,
 
 	/*
 	 * "ec" implies in-system programming on a live system, so
