07d8f3a9fb9000a0a27ad016027dacaf0d53ece9
Firas Sammoura <fsammoura@google.com>
2022-10-18 22:41:27 +0000

    flashrom: Update host command print formatting
    Modify the format of how the host commands are being printed in the
    program error messages and program debug messages. Change zero padding
    from 2 to 4 to conform with the command, which is defined as a 16-bit
    value.
    
    BUG=b:253233116
    BRANCH=None
    TEST=tast run <DUT> firmware.FpUpdater
    
    Signed-off-by: Firas Sammoura <fsammoura@google.com>
    Change-Id: I6ee3ec6275210516f20a3ad0d58166c8d11cc8d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/3965278
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>

--- cros_ec_dev.c
+++ cros_ec_dev.c
@@ -203,12 +203,12 @@ static int __cros_ec_command_dev(int command, int version,
 		cmd.result = 0;
 	}
 	if (ret < 0) {
-		msg_perr("%s(): Command 0x%02x failed: %d, errno=%d\n",
+		msg_perr("%s(): Command 0x%04x failed: %d, errno=%d\n",
 			__func__, command, ret, errno);
 		return -EC_RES_ERROR;
 	}
 	if (cmd.result) {
-		msg_pdbg("%s(): Command 0x%02x returned result: %d\n",
+		msg_pdbg("%s(): Command 0x%04x returned result: %d\n",
 			 __func__, command, cmd.result);
 		return -cmd.result;
 	}
@@ -298,13 +298,13 @@ static int __cros_ec_command_dev_v2(int command, int version,
 		s_cmd->result = 0;
 	}
 	if (ret < 0) {
-		msg_perr("%s(): Command 0x%02x failed: %d, errno=%d\n",
+		msg_perr("%s(): Command 0x%04x failed: %d, errno=%d\n",
 			__func__, command, ret, errno);
 		free(s_cmd);
 		return -EC_RES_ERROR;
 	}
 	if (s_cmd->result) {
-		msg_pdbg("%s(): Command 0x%02x returned result: %d\n",
+		msg_pdbg("%s(): Command 0x%04x returned result: %d\n",
 			 __func__, command, s_cmd->result);
 		ret = -s_cmd->result;
 		free(s_cmd);
