e2be9ce368d0e0d61622af2582419efeb2f541c8
Edward O'Callaghan <quasisec@google.com>
2022-11-25 13:32:09 +1100

    cros_ec.c: Mark ec chip as tested for WP
    BUG=b:258755442
    BRANCH=none
    TEST=none
    
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Change-Id: I08b1f19588df57a2546aefe815208f549c2b1128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/4056558
    Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>

--- cros_ec.c
+++ cros_ec.c
@@ -877,7 +877,7 @@ int cros_ec_probe_size(struct flashctx *flash)
 		memcpy(chip_name, chip_info.name, sizeof(chip_name));
 		flash->chip->vendor = chip_vendor;
 		flash->chip->name = chip_name;
-		flash->chip->tested = TEST_OK_PREW;
+		flash->chip->tested = TEST_OK_PREWB;
 	} else {
 		const struct flashchip *f;
 		uint32_t mfg = spi_info.jedec[0];
