CodeLinaro-mirror/la_platform_external_flashrom

commit e45353c70ef512ff0218813c749af6b2867046f2

flashrom.c: Contain access error in eraser helper
Previously erase_and_write_block_helper() would return SPI_ACCESS_DENIED
if an write/erase operation failed or was not allowed by check_access().

The error code would then be discarded in walk_eraseregions().

Just return 0 from erase_and_write_block_helper() if access is denied.

BUG=b:235155342
BRANCH=none
TEST=builds

Signed-off-by: Nikolai Artemiev <nartemiev@google.com>
Change-Id: I823fbbb8f59bec6fee09f1756f1cecb3b8482cc9
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/3989764
Reviewed-by: Evan Benn <evanbenn@chromium.org>
Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>
Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>

flashrom.c
@@ -1418,13 +1418,9 @@ static int walk_eraseregions(struct flashctx *flash,
 			};
 			erasefunc_t *erase_func = lookup_erase_func_ptr(eraser);
 			rc = per_blockfn(flash, &info, erase_func);
+			if (rc)
+				return rc;
 
-			if (rc) {
-				if (rc == SPI_ACCESS_DENIED)
-					rc = 0;
-				else
-					return rc;
-			}
 			base += pu->block_size;
 		}
 	}
@@ -1432,6 +1428,13 @@ static int walk_eraseregions(struct flashctx *flash,
 	return rc;
 }
 
+/*
+ * Helper function called on each block to be erased and written.
+ *
+ * Returns 0 if erase and write operations succeed or if they are skipped
+ *         because the block is in a non-writable region.
+ * Returns non-0 error code if erase or write operations fail unexpectedly.
+ */
 static int erase_and_write_block_helper(struct flashctx *const flash,
 					const struct walk_info *const info,
 					const erasefn_t erasefn)
@@ -1449,7 +1452,7 @@ static int erase_and_write_block_helper(struct flashctx *const flash,
 
 		if (check_access(flash, info->erase_start, erase_len, 1)) {
 			msg_cdbg(" DENIED");
-			return SPI_ACCESS_DENIED;
+			return 0;
 		}
 
 		ret = erasefn(flash, info->erase_start, erase_len);
@@ -1480,7 +1483,7 @@ static int erase_and_write_block_helper(struct flashctx *const flash,
 
 		if (check_access(flash, info->erase_start + starthere, lenhere, 1)) {
 			msg_cdbg(" DENIED");
-			return SPI_ACCESS_DENIED;
+			return 0;
 		}
 
 		/* Needs the partial write function signature. */

commit ed36023839543c54540b90f89a65233a3286f6fa

flashrom.c: Fix cros_ec erase path regression
The commit e45353c70e regressed Nereid in build 15257.0.0.
It was not noticed that ichspi is not the only thing that
can return a SPI_ACCESS_DENIED result that needs to be
masked (ignored). Restore previous behavior of this
technical debt.

BUG=b:235155342,b:260365841
BRANCH=none
TEST=builds

Signed-off-by: Edward O'Callaghan <quasisec@google.com>
Change-Id: Ia5b90d31aae13e857948e9d5f5b0dbd3d0f84167
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/4055242
Reviewed-by: Sam McNally <sammc@chromium.org>
Tested-by: Edward O'Callaghan <quasisec@chromium.org>
Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>
Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

flashrom.c
@@ -1397,8 +1397,12 @@ static int walk_eraseregions(struct flashctx *flash,
 			};
 			erasefunc_t *erase_func = lookup_erase_func_ptr(eraser);
 			rc = per_blockfn(flash, &info, erase_func);
-			if (rc)
-				return rc;
+			if (rc) {
+				if (rc == SPI_ACCESS_DENIED) /* from cros_ec erase path. */
+					rc = 0;
+				else
+					return rc;
+			}
 
 			base += pu->block_size;
 		}
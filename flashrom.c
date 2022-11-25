ed36023839543c54540b90f89a65233a3286f6fa
Edward O'Callaghan <quasisec@google.com>
2022-11-25 14:20:50 +1100

    flashrom.c: Fix cros_ec erase path regression
    The commit e45353c70e regressed Nereid in build 15257.0.0.
    It was not noticed that ichspi is not the only thing that
    can return a SPI_ACCESS_DENIED result that needs to be
    masked (ignored). Restore previous behavior of this
    technical debt.
    
    BUG=b:235155342,b:260365841
    BRANCH=none
    TEST=builds
    
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Change-Id: Ia5b90d31aae13e857948e9d5f5b0dbd3d0f84167
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/4055242
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>
    Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

--- flashrom.c
+++ flashrom.c
@@ -1397,8 +1397,12 @@ static int walk_eraseregions(struct flashctx *flash,
 			};
 			erasefunc_t *erase_func = lookup_erase_func_ptr(eraser);
 			rc = per_blockfn(flash, &info, erase_func);
-			if (rc)
-				return rc;
+			if (rc) {
+				if (rc == SPI_ACCESS_DENIED) /* from cros_ec erase path. */
+					rc = 0;
+				else
+					return rc;
+			}
 
 			base += pu->block_size;
 		}
