71ede00efd482f497822841375211fcd5cf00d6b
Edward O'Callaghan <quasisec@google.com>
2022-12-15 12:49:50 +1100

    cros_ec.c: Make cros_ec_block_erase() ret 0 on spi access denied
    The 'SPI_ACCESS_DENIED' define is bespoke to ChromeOS where it
    is used to signify permissions errors only to ignore them at
    the call site. Just return success of zero instead to avoid special
    call site handling.
    
    BUG=none
    BRANCH=none
    TEST=builds
    
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Change-Id: I8a21bcd88ae3740149d5eb25f339660d2b018333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/4109848
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>

--- flashrom.c
+++ flashrom.c
@@ -1399,10 +1399,7 @@ static int walk_eraseregions(struct flashctx *flash,
 			erasefunc_t *erase_func = lookup_erase_func_ptr(eraser);
 			rc = per_blockfn(flash, &info, erase_func);
 			if (rc) {
-				if (rc == SPI_ACCESS_DENIED) /* from cros_ec erase path. */
-					rc = 0;
-				else
-					return rc;
+				return rc;
 			}
 
 			base += pu->block_size;
@@ -1449,10 +1446,7 @@ static int erase_and_write_block_helper(struct flashctx *const flash,
 
 		ret = erasefn(flash, info->erase_start, erase_len);
 		if (ret) {
-			if (ret == SPI_ACCESS_DENIED) /* from cros_ec erase path. */
-				msg_cdbg(" DENIED");
-			else
-				msg_cerr(" ERASE_FAILED\n");
+			msg_cerr(" ERASE_FAILED\n");
 			return ret;
 		}
 
