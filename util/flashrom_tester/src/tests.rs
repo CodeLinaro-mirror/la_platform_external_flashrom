d6fd8124142ca2173ed66c1903ef4cba80363f4d
Evan Benn <evanbenn@chromium.org>
2023-01-16 16:12:44 +1100

    BACKPORT: flashrom_tester: Rewrite IOOpts to support more operations
    flashrom cli supports include regions for all of read write and verify,
    as well as omitting the read/write/verify file if an include region with
    file is specified. Use an enum to allow only one operation at a time.
    Unify the read and write region implementations.
    
    BUG=b:235916336
    BRANCH=None
    TEST=None
    
    (cherry picked from commit 72e62750c8734bcf2d99da2dc3b5dc1d0cb38b5a)
    
    Original-Change-Id: I1cb46bb1b26949fd9c19949c43708a8b652e00da
    Original-Signed-off-by: Evan Benn <evanbenn@chromium.org>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/71973
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Original-Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    
    Signed-off-by: Nikolai Artemiev <nartemiev@google.com>
    Change-Id: I5b32979949348505556ba6133fda2cba0e11bb22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/4258806
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

--- util/flashrom_tester/src/tests.rs
+++ util/flashrom_tester/src/tests.rs
@@ -283,14 +283,14 @@ fn partial_lock_test(section: LayoutNames) -> impl Fn(&mut TestEnv) -> TestResul
         env.wp.set_hw(true)?;
 
         // Check that we cannot write to the protected region.
-        let rws = flashrom::ROMWriteSpecifics {
-            layout_file: Some(&env.layout_file),
-            write_file: Some(env.random_data_file()),
-            name_file: Some(wp_section_name),
-        };
-        if env.cmd.write_file_with_layout(&rws).is_ok() {
-            info!(
-                "Section should be locked but flashrom write returned Ok. This may be incorrect; verifying"
+        if env
+            .cmd
+            .write_from_file_region(env.random_data_file(), wp_section_name, &env.layout_file)
+            .is_ok()
+        {
+            return Err(
+                "Section should be locked, should not have been overwritable with random data"
+                    .into(),
             );
         }
         if !env.is_golden() {
@@ -300,12 +300,11 @@ fn partial_lock_test(section: LayoutNames) -> impl Fn(&mut TestEnv) -> TestResul
         // Check that we can write to the non protected region.
         let (non_wp_section_name, _, _) =
             utils::layout_section(env.layout(), section.get_non_overlapping_section());
-        let rws = flashrom::ROMWriteSpecifics {
-            layout_file: Some(&env.layout_file),
-            write_file: Some(env.random_data_file()),
-            name_file: Some(non_wp_section_name),
-        };
-        env.cmd.write_file_with_layout(&rws)?;
+        env.cmd.write_from_file_region(
+            env.random_data_file(),
+            non_wp_section_name,
+            &env.layout_file,
+        )?;
 
         Ok(())
     }
