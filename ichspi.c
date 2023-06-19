e8041808777a4a734b64ed0a3944a5fd44bbba2a
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-06-19 09:36:52 +0200

    UPSTREAM: ichspi: Add RaptorPoint PCH support
    Based on public Intel 700 Series PCH datasheet, DOC 743835 rev 004.
    
    The IDs of IoT chipset SKUs (ending with E) can only be found in "12th
    Gen Intel® Core™ Processors Family (Formerly Known as Alder Lake -S)
    for IoT Platforms External Design Specification (EDS) Addendum" DOC
    634528 rev 2.7 (NDA).
    
    TEST=Probe flash on Z790 chipset. Run the ich_descriptors_tool and
    check the output is correct as expected.
    
    (cherry picked from commit 11d5c1750f29229f6e83bbb062233d24fb2efcf0)
    
    Original-Change-Id: I13ac52d5400c0e2260e12d605077fc2182c379ef
    Original-Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/83854
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Original-Reviewed-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
    GitOrigin-RevId: 11d5c1750f29229f6e83bbb062233d24fb2efcf0
    Cr-Build-Id: 8735830120951844497
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8735830120951844497
    Copybot-Job-Name: flashrom-main-copybot-downstream
    Change-Id: I8b54284148827e43869715ea8ca3f91ca5383c32
    Signed-off-by: chromeos-ci-prod <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/5843200
    Reviewed-by: Hsuan Ting Chen <roccochen@chromium.org>
    Commit-Queue: Hsuan Ting Chen <roccochen@chromium.org>

--- ichspi.c
+++ ichspi.c
@@ -2152,6 +2152,7 @@ static void init_chipset_properties(struct swseq_data *swseq, struct hwseq_data 
 	case CHIPSET_400_SERIES_COMET_POINT:
 	case CHIPSET_500_SERIES_TIGER_POINT:
 	case CHIPSET_600_SERIES_ALDER_POINT:
+	case CHIPSET_700_SERIES_RAPTOR_POINT:
 	case CHIPSET_APOLLO_LAKE:
 	case CHIPSET_GEMINI_LAKE:
 	case CHIPSET_JASPER_LAKE:
@@ -2193,6 +2194,7 @@ static void init_chipset_properties(struct swseq_data *swseq, struct hwseq_data 
 	case CHIPSET_400_SERIES_COMET_POINT:
 	case CHIPSET_500_SERIES_TIGER_POINT:
 	case CHIPSET_600_SERIES_ALDER_POINT:
+	case CHIPSET_700_SERIES_RAPTOR_POINT:
 	case CHIPSET_APOLLO_LAKE:
 	case CHIPSET_GEMINI_LAKE:
 	case CHIPSET_JASPER_LAKE:
@@ -2256,6 +2258,7 @@ static int init_ich_default(const struct programmer_cfg *cfg, void *spibar, enum
 	case CHIPSET_400_SERIES_COMET_POINT:
 	case CHIPSET_500_SERIES_TIGER_POINT:
 	case CHIPSET_600_SERIES_ALDER_POINT:
+	case CHIPSET_700_SERIES_RAPTOR_POINT:
 	case CHIPSET_APOLLO_LAKE:
 	case CHIPSET_GEMINI_LAKE:
 	case CHIPSET_JASPER_LAKE:
@@ -2337,6 +2340,7 @@ static int init_ich_default(const struct programmer_cfg *cfg, void *spibar, enum
 		case CHIPSET_400_SERIES_COMET_POINT:
 		case CHIPSET_500_SERIES_TIGER_POINT:
 		case CHIPSET_600_SERIES_ALDER_POINT:
+		case CHIPSET_700_SERIES_RAPTOR_POINT:
 		case CHIPSET_APOLLO_LAKE:
 		case CHIPSET_GEMINI_LAKE:
 		case CHIPSET_JASPER_LAKE:
@@ -2378,6 +2382,7 @@ static int init_ich_default(const struct programmer_cfg *cfg, void *spibar, enum
 		case CHIPSET_400_SERIES_COMET_POINT:
 		case CHIPSET_500_SERIES_TIGER_POINT:
 		case CHIPSET_600_SERIES_ALDER_POINT:
+		case CHIPSET_700_SERIES_RAPTOR_POINT:
 		case CHIPSET_APOLLO_LAKE:
 		case CHIPSET_GEMINI_LAKE:
 		case CHIPSET_JASPER_LAKE:
@@ -2417,6 +2422,7 @@ static int init_ich_default(const struct programmer_cfg *cfg, void *spibar, enum
 	     ich_gen == CHIPSET_400_SERIES_COMET_POINT ||
 	     ich_gen == CHIPSET_500_SERIES_TIGER_POINT ||
 	     ich_gen == CHIPSET_600_SERIES_ALDER_POINT ||
+	     ich_gen == CHIPSET_700_SERIES_RAPTOR_POINT ||
 	     ich_gen == CHIPSET_C740_SERIES_EMMITSBURG)) {
 		msg_pdbg("Enabling hardware sequencing by default for 100+ series PCH.\n");
 		ich_spi_mode = ich_hwseq;
