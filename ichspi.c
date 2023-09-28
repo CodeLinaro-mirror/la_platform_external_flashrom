58b496dad509417369634af6c95aa43e9daa7e40
Patrick Rudolph <patrick.rudolph@9elements.com>
2023-09-28 15:58:48 +0200

    UPSTREAM: ichspi: Add support for C740 PCH
    Clean commit 51e1d0e4b7670e5822560acc724a6a8dd00b6af4
    'Add support for Intel Emmitsburg PCH' which broke
    CHIPSET_5_SERIES_IBEX_PEAK detection and which assumes C740 is the same
    as C620, while its more a close relative to Intel's H570 PCH.
    
    Based on Intel SPI Programming Guide #619386.
    
          using the 'internal' programmer.
          'linux_mtd' programmer.
    (cherry picked from commit 1dd7c88f41e49a076a189154928030d37ca00d26)
    
    Original-Test: Run on Intel ArcherCity CRB with Intel's C741 PCH
    Original-Test: Run on BMC and accessed the SPI flash chip over
    Original-Change-Id: I80eebc0fcc14de9df823aceaee77870ad136f94a
    Original-Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/78186
    Original-Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    GitOrigin-RevId: 1dd7c88f41e49a076a189154928030d37ca00d26
    Change-Id: If1ba017adb72b131f2fc68aeb15b9473b78f75e0
    Signed-off-by: chromeos-ci-prod <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/4976098
    Reviewed-by: Hsuan Ting Chen <roccochen@chromium.org>
    Commit-Queue: Hsuan Ting Chen <roccochen@chromium.org>

--- ichspi.c
+++ ichspi.c
@@ -458,6 +458,7 @@ static void prettyprint_ich9_reg_hsfs(uint16_t reg_val, enum ich_chipset ich_gen
 	switch (ich_gen) {
 	case CHIPSET_100_SERIES_SUNRISE_POINT:
 	case CHIPSET_C620_SERIES_LEWISBURG:
+	case CHIPSET_C740_SERIES_EMMITSBURG:
 	case CHIPSET_300_SERIES_CANNON_POINT:
 	case CHIPSET_400_SERIES_COMET_POINT:
 	case CHIPSET_500_SERIES_TIGER_POINT:
@@ -471,6 +472,7 @@ static void prettyprint_ich9_reg_hsfs(uint16_t reg_val, enum ich_chipset ich_gen
 	switch (ich_gen) {
 	case CHIPSET_100_SERIES_SUNRISE_POINT:
 	case CHIPSET_C620_SERIES_LEWISBURG:
+	case CHIPSET_C740_SERIES_EMMITSBURG:
 	case CHIPSET_300_SERIES_CANNON_POINT:
 	case CHIPSET_400_SERIES_COMET_POINT:
 	case CHIPSET_500_SERIES_TIGER_POINT:
@@ -493,6 +495,7 @@ static void prettyprint_ich9_reg_hsfc(uint16_t reg_val, enum ich_chipset ich_gen
 	switch (ich_gen) {
 	case CHIPSET_100_SERIES_SUNRISE_POINT:
 	case CHIPSET_C620_SERIES_LEWISBURG:
+	case CHIPSET_C740_SERIES_EMMITSBURG:
 	case CHIPSET_300_SERIES_CANNON_POINT:
 	case CHIPSET_400_SERIES_COMET_POINT:
 	case CHIPSET_500_SERIES_TIGER_POINT:
@@ -2096,6 +2099,7 @@ static void init_chipset_properties(struct swseq_data *swseq, struct hwseq_data 
 	switch (ich_gen) {
 	case CHIPSET_100_SERIES_SUNRISE_POINT:
 	case CHIPSET_C620_SERIES_LEWISBURG:
+	case CHIPSET_C740_SERIES_EMMITSBURG:
 	case CHIPSET_300_SERIES_CANNON_POINT:
 	case CHIPSET_400_SERIES_COMET_POINT:
 	case CHIPSET_500_SERIES_TIGER_POINT:
@@ -2133,6 +2137,7 @@ static void init_chipset_properties(struct swseq_data *swseq, struct hwseq_data 
 		*num_freg = 10;
 		break;
 	case CHIPSET_C620_SERIES_LEWISBURG:
+	case CHIPSET_C740_SERIES_EMMITSBURG:
 		*num_freg = 12;	/* 12 MMIO regs, but 16 regions in FD spec */
 		break;
 	case CHIPSET_300_SERIES_CANNON_POINT:
@@ -2196,6 +2201,7 @@ static int init_ich_default(const struct programmer_cfg *cfg, void *spibar, enum
 	switch (ich_gen) {
 	case CHIPSET_100_SERIES_SUNRISE_POINT:
 	case CHIPSET_C620_SERIES_LEWISBURG:
+	case CHIPSET_C740_SERIES_EMMITSBURG:
 	case CHIPSET_300_SERIES_CANNON_POINT:
 	case CHIPSET_400_SERIES_COMET_POINT:
 	case CHIPSET_500_SERIES_TIGER_POINT:
@@ -2276,6 +2282,7 @@ static int init_ich_default(const struct programmer_cfg *cfg, void *spibar, enum
 		case CHIPSET_ICH8:
 		case CHIPSET_100_SERIES_SUNRISE_POINT:
 		case CHIPSET_C620_SERIES_LEWISBURG:
+		case CHIPSET_C740_SERIES_EMMITSBURG:
 		case CHIPSET_300_SERIES_CANNON_POINT:
 		case CHIPSET_400_SERIES_COMET_POINT:
 		case CHIPSET_500_SERIES_TIGER_POINT:
@@ -2315,6 +2322,7 @@ static int init_ich_default(const struct programmer_cfg *cfg, void *spibar, enum
 		case CHIPSET_ICH8:
 		case CHIPSET_100_SERIES_SUNRISE_POINT:
 		case CHIPSET_C620_SERIES_LEWISBURG:
+		case CHIPSET_C740_SERIES_EMMITSBURG:
 		case CHIPSET_300_SERIES_CANNON_POINT:
 		case CHIPSET_400_SERIES_COMET_POINT:
 		case CHIPSET_500_SERIES_TIGER_POINT:
@@ -2356,7 +2364,8 @@ static int init_ich_default(const struct programmer_cfg *cfg, void *spibar, enum
 	     ich_gen == CHIPSET_300_SERIES_CANNON_POINT ||
 	     ich_gen == CHIPSET_400_SERIES_COMET_POINT ||
 	     ich_gen == CHIPSET_500_SERIES_TIGER_POINT ||
-	     ich_gen == CHIPSET_600_SERIES_ALDER_POINT)) {
+	     ich_gen == CHIPSET_600_SERIES_ALDER_POINT ||
+	     ich_gen == CHIPSET_C740_SERIES_EMMITSBURG)) {
 		msg_pdbg("Enabling hardware sequencing by default for 100+ series PCH.\n");
 		ich_spi_mode = ich_hwseq;
 	}
