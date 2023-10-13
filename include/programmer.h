4d0acdf62796eed1df2df061f73c05b2f81c19b0
Hsuan Ting Chen <roccochen@chromium.org>
2023-10-13 02:50:42 +0000

    Revert "cros_alias.c: Drop technical debt"
    This reverts commit f2797baa94fb09019191ed06d1a44cf3d3e5e8c5.
    
    Reason for revert: We need to give our partners some more time to catch up on this change
    
    Original change's description:
    > cros_alias.c: Drop technical debt
    >
    > Drop `-p host` ChromeOS miss-feature and use `-p internal` instead.
    >
    > BUG=b:296978620
    > BRANCH=none
    > TEST=internal is directly equiv to host.
    >
    > Cq-Depend: chromium:4933120
    > Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    > Change-Id: I2bbab882e831f360980470ab66a3e88b97efad20
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/4805144
    > Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>
    > Reviewed-by: Hsuan Ting Chen <roccochen@chromium.org>
    > Tested-by: Edward O'Callaghan <quasisec@chromium.org>
    > Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>
    
    BUG=b:296978620
    
    Change-Id: Ice4ea830b624ea28c74f869bb1f21bf4dd654695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/4938170
    Auto-Submit: Hsuan Ting Chen <roccochen@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- include/programmer.h
+++ include/programmer.h
@@ -109,6 +109,7 @@ extern const struct programmer_entry programmer_usbblaster_spi;
 extern const struct programmer_entry programmer_dirtyjtag_spi;
 
 extern const struct programmer_entry programmer_cros_ec;
+extern const struct programmer_entry programmer_google_host_alias;
 
 int programmer_init(const struct programmer_entry *prog, const char *param);
 int programmer_shutdown(void);
