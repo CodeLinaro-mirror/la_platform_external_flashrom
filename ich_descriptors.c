90d91789006388d3bbb5e7b9870ab4548c0e679c
roccochen@chromium.com <roccochen@chromium.org>
2024-03-19 08:19:45 +0800

    FROMLIST: ich: Add names for region 5, 9, 10, 11, 12, 15
    Add Region 9 for Intel Meteor Lake; update missing regions.
    
    * Include Region 9 as officially required for Intel Meteor Lake platform.
    * Incorporate missing region names from https://github.com/coreboot/coreboot/blob/main/util/ifdtool/ifdtool.c for completeness.
    
    Region 5: Device Expansion (DE or DevExp)
    Region 9: Device Expansion 2 (DE2 or DevExp2)
    Region 10: Innovation Engine (IE)
    Region 11: 10 GbE 0
    Region 12: 10 GbE 1
    Region 15: PTT
    
    (cherry picked from commit 846e507c66601a81b7ff4297ee0fb857f1e508dc)
    
    BUG=b:319773700
    TEST=none
    
    Change-Id: I3b164ce4ae84bfd523fcd8be416c5d13183ed632
    Signed-off-by: roccochen@chromium.com <roccochen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/5377588
    Commit-Queue: Jian-Jia Su <jjsu@chromium.org>
    Reviewed-by: Jian-Jia Su <jjsu@chromium.org>

--- ich_descriptors.c
+++ ich_descriptors.c
@@ -518,24 +518,24 @@ void prettyprint_ich_descriptor_master(const enum ich_chipset cs, const struct i
 		}
 
 		size_t num_regions;
-		msg_pdbg2("      FD  BIOS  ME  GbE  Pltf Reg5 Reg6 Reg7  EC  Reg9");
+		msg_pdbg2("       FD    BIOS    ME    GbE    Pltf    DE   BIOS2   Reg7    EC    DE2  ");
 		if (cs == CHIPSET_100_SERIES_SUNRISE_POINT) {
 			num_regions = 10;
 			msg_pdbg2("\n");
 		} else {
 			num_regions = 16;
-			msg_pdbg2(" RegA RegB RegC RegD RegE RegF\n");
+			msg_pdbg2("  IE   10GbE0 10GbE1  RegD   RegE   PTT  \n");
 		}
 		for (i = 0; i < nm; i++) {
 			const unsigned int ext_region_start = 12;
 			size_t j;
 			msg_pdbg2("%-4s", master_names[i]);
 			for (j = 0; j < (size_t)min(num_regions, ext_region_start); j++)
-				msg_pdbg2("  %c%c ",
+				msg_pdbg2("   %c%c  ",
 					  desc->master.mstr[i].read & (1 << j) ? 'r' : ' ',
 					  desc->master.mstr[i].write & (1 << j) ? 'w' : ' ');
 			for (j = ext_region_start; j < num_regions; j++)
-				msg_pdbg2("  %c%c ",
+				msg_pdbg2("   %c%c  ",
 					  desc->master.mstr[i].ext_read & (1 << (j - ext_region_start)) ? 'r' : ' ',
 					  desc->master.mstr[i].ext_write & (1 << (j - ext_region_start)) ? 'w' : ' ');
 			msg_pdbg2("\n");
