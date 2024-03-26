270dc43144edae23800d67e23a804feb7ec87782
Peter Marheine <pmarheine@chromium.org>
2024-03-27 10:54:01 +1100

    BACKPORT: UPSTREAM: udelay: only use OS time for delays, except on DOS
    As proposed on the mailing list ("RFC: remove the calibrated delay
    loop" [1]), this removes the calibrated delay loop and uses OS-based
    timing functions for all delays because the calibrated delay loop can
    delay for shorter times than intended.
    
    When sleeping this now uses nanosleep() unconditionally, since usleep
    was only used on DOS (where DJGPP lacks nanosleep).  When busy-looping,
    it uses clock_gettime() with CLOCK_MONOTONIC or CLOCK_REALTIME depending
    on availability, and gettimeofday() otherwise.
    
    The calibrated delay loop is retained for DOS only, because timer
    resolution on DJGPP is only about 50 milliseconds. Since typical delays
    in flashrom are around 10 microseconds, using OS timing there would
    regress performance by around 500x. The old implementation is reused
    with some branches removed based on the knowledge that timer resolution
    will not be better than about 50 milliseconds.
    
    Tested by reading and writing flash on several Intel and AMD systems:
    
     * Lenovo P920 (Intel C620, read/verify only)
     * "nissa" chromebook (Intel Alder Lake-N)
     * "zork" chromebook (AMD Zen+)
    
    [1]: https://mail.coreboot.org/hyperkitty/list/flashrom@flashrom.org/thread/HFH6UHPAKA4JDL4YKPSQPO72KXSSRGME/
    
    BUG=none
    TEST=none
    
    (cherry picked from commit 183208b5cb397c9b58762900502e6b1b0a358ae6)
    
    Original-Signed-off-by: Peter Marheine <pmarheine@chromium.org>
    Original-Change-Id: I7ac5450d194a475143698d65d64d8bcd2fd25e3f
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/81545
    Original-Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Brian Norris <briannorris@chromium.org>
    GitOrigin-RevId: 183208b5cb397c9b58762900502e6b1b0a358ae6
    Backport-notes: Solve conflicts in meson due to ChromeOS-specific files
    Change-Id: I0f9556292328e4019794ec017ac6ebaa49f24677
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/5550675
    Reviewed-by: Jian-Jia Su <jjsu@chromium.org>
    Commit-Queue: Jian-Jia Su <jjsu@chromium.org>

--- include/programmer.h
+++ include/programmer.h
@@ -228,8 +228,6 @@ extern const struct board_info laptops_known[];
 #endif
 
 /* udelay.c */
-void myusec_delay(unsigned int usecs);
-void myusec_calibrate_delay(void);
 void internal_sleep(unsigned int usecs);
 void default_delay(unsigned int usecs);
 
