3afd68402e306e35fd2abb351bf5543a256364f0
Hsuan Ting Chen <roccochen@chromium.org>
2024-05-22 17:45:02 +0800

    FROMLIST: flashchips: Correct feature_bits for MX25L128*
    In CB:81792 (CL:5471748) we add write-protect support for MX25L12833F,
    MX25L12835F/MX25L12873F, and MX25L12845E/MX25L12865E.
    
    However, that CL contained a bug that it didn't set the feature_bits
    correctly. We need to add:
    
    * Add FEATURE_CFGR and FEATURE_SCUR for MX25L12833F
    * Add FEATURE_CFGR and FEATURE_SCUR for MX25L12835F/MX25L12873F
    * Add FEATURE_SCUR for MX25L12845E/MX25L12865E
    
    (am from https://review.coreboot.org/c/flashrom/+/82605/1)
    
    BUG=b:332486637
    TEST=In a host connect to ADL-n ChromeOS via servo, with this patch,
         flashrom -p raiden_debug_spi:target=AP,custom_rst=true
         ,serial=$(dut-control -o ccd_serialname -p 9996) --wp-status
         returns correct WP status.
    TEST=without this patch, returns error with
         "Cannot read SECURITY: unsupported by chip"
    
    Change-Id: Id35bd939ee2d2249a547edfa3bccf651f5ad5d86
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/5556910
    Reviewed-by: Jian-Jia Su <jjsu@chromium.org>

--- flashchips.c
+++ flashchips.c
@@ -9012,7 +9012,7 @@ const struct flashchip flashchips[] = {
 		.total_size	= 16384,
 		.page_size	= 256,
 		/* OTP: 1KB total; enter 0xB1, exit 0xC1 */
-		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP,
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_CFGR | FEATURE_SCUR,
 		.tested		= TEST_OK_PREWB,
 		.probe		= PROBE_SPI_RDID,
 		.probe_timing	= TIMING_ZERO,
@@ -9059,7 +9059,7 @@ const struct flashchip flashchips[] = {
 		.total_size	= 16384,
 		.page_size	= 256,
 		/* OTP: 512B total; enter 0xB1, exit 0xC1 */
-		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP,
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_CFGR | FEATURE_SCUR,
 		.tested		= TEST_OK_PREW,
 		.probe		= PROBE_SPI_RDID,
 		.probe_timing	= TIMING_ZERO,
@@ -9106,7 +9106,7 @@ const struct flashchip flashchips[] = {
 		.total_size	= 16384,
 		.page_size	= 256,
 		/* OTP: 512B total; enter 0xB1, exit 0xC1 */
-		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP,
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_SCUR,
 		.tested		= TEST_OK_PREW,
 		.probe		= PROBE_SPI_RDID,
 		.probe_timing	= TIMING_ZERO,
