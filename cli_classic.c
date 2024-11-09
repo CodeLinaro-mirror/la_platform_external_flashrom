09936943b093765909b30abae37c1ec79c3c7c2c
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com-copybot-pick>
2024-11-09 11:42:04 +0100

    UPSTREAM: Rename cli_classic.h to a more adequate cli_getop.h
    The header only defines getop related stuff so it seems more intuitive
    this way.
    
    (cherry picked from commit a3a8ae7212ec643b6c7a461311b91ff2e403c990)
    
    Original-Change-Id: Iaceeabedc26e93147d8122376d88e730aad1e355
    Original-Signed-off-by: Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/85072
    Original-Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    GitOrigin-RevId: a3a8ae7212ec643b6c7a461311b91ff2e403c990
    Cr-Build-Id: 8722963462681619937
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8722963462681619937
    Copybot-Job-Name: flashrom-main-copybot-downstream
    Change-Id: Idf9859aaa68b26b996a4905b6d516b2c936f0c06
    Signed-off-by: chromeos-ci-prod <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/6022544
    Reviewed-by: Hsuan Ting Chen <roccochen@chromium.org>
    Commit-Queue: Hsuan Ting Chen <roccochen@chromium.org>
    Tested-by: Hsuan Ting Chen <roccochen@chromium.org>

--- cli_classic.c
+++ cli_classic.c
@@ -24,7 +24,8 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <cli_classic.h>
+#include <stdint.h>
+#include <cli_getopt.h>
 #include "flash.h"
 #include "flashchips.h"
 #include "fmap.h"
