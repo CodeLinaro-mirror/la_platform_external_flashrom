87a81a94672a1f4e41597f3796f682a6867cd4c5
Hsuan Ting Chen <roccochen@chromium.org>
2025-12-09 19:22:54 +0800

    CHROMIUM: Add --ignore-lock and auto-skip lock for remote programmers
    Currently, flashrom acquires a system-wide lock (big lock) on the host
    even when using remote programmers like 'raiden_debug_spi' (Servo).
    This prevents parallel flashing of multiple DUTs connected to the same
    labstation, which is a significant bottleneck in lab environments.
    
    The big lock is intended to arbitrate access to local host hardware
    (e.g., internal SPI bus), but remote programmers communicate via USB
    and do not contend for these resources.
    
    Furthermore, 'raiden_debug_spi' already enforces mutual exclusion at
    the device level via libusb's interface claiming mechanism. If multiple
    processes try to access the same Servo, the second attempt will fail
    at the USB layer. Thus, the system-wide lock is redundant and
    unnecessarily restrictive for this use case.
    
    This patch:
    1. Adds a '--ignore-lock' CLI option to manually bypass the lock.
    2. Automatically skips the lock in 'programmer_init' for
       'raiden_debug_spi'. This ensures that libflashrom clients
       (like vboot/futility) benefit from this fix without requiring API
       changes or recompilation.
    
    BUG=b:467275316, b:466103746
    TEST=sudo chmod 000 /run/lock/firmware_utility_lock
         # CLI manual test:
         flashrom -p internal -VVV #fail
         flashrom -p raiden_debug_spi:target=AP -VVV #succ (auto-skip)
         flashrom -p raiden_debug_spi:target=AP --ignore-lock -VVV #succ
         # Library/Integration test:
         futility flash --wp-status #fail
         futility flash --servo --wp-status #succ
    
    Change-Id: I6228252b23483866539ef3b4bfa0543b14be0f4b
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/7240889
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>

--- include/programmer.h
+++ include/programmer.h
@@ -298,6 +298,7 @@ struct decode_sizes {
 // FIXME: These need to be local, not global
 extern struct decode_sizes max_rom_decode;
 extern bool programmer_may_write;
+extern bool ignore_lock;
 extern uintptr_t flashbase; /* used in programmer_enable.c */
 char *extract_programmer_param_str(const struct programmer_cfg *cfg, const char *param_name);
 
